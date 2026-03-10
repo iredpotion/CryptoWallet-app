f6d1c8c321e56230582d38489de34453f296e96d
Pedro Toni <pedrotoni60@gmail.com>
2026-03-10 19:56:28 -0300

    Bug fix!

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,9 +8,11 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors();
+
+  // O Zod (usado no Login/Signup) não emite metadados compreendidos pelo class-validator.
+  // Portanto, removemos whitelist e forbidNonWhitelisted para que as requisições
+  // do Zod não cheguem vazias no controller e tomem erro 400 Bad Request.
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
     transform: true
   }));
 
